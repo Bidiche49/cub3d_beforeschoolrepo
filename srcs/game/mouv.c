9189f0836eb3aab380f8a9101cc678fdb1f2b286
audrye <augustin.drye@gmail.com>
2023-12-12 20:19:32 +0100

    collision faite

--- srcs/game/mouv.c
+++ srcs/game/mouv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouv.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntardy <ntardy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: audrye <audrye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 16:34:24 by audrye            #+#    #+#             */
-/*   Updated: 2023/12/12 20:01:10 by ntardy           ###   ########.fr       */
+/*   Updated: 2023/12/12 20:16:40 by audrye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,20 @@ void	mouv_press_w(t_data *data)
 {
 	data->player.posY += cos(data->player.dir) * 0.1f;
 	data->player.posX += sin(data->player.dir) * 0.1f;
+	if (data->map[(int)data->player.posY][(int)data->player.posX] == '1')
+	{
+		data->player.posY -= cos(data->player.dir) * 0.1f;
+		data->player.posX -= sin(data->player.dir) * 0.1f;
+	}
 }
 
 void	mouv_press_s(t_data *data)
 {
 	data->player.posY -= cos(data->player.dir) * 0.1f;
 	data->player.posX -= sin(data->player.dir) * 0.1f;
+	if (data->map[(int)data->player.posY][(int)data->player.posX] == '1')
+	{
+		data->player.posY += cos(data->player.dir) * 0.1f;
+		data->player.posX += sin(data->player.dir) * 0.1f;
+	}
 }
